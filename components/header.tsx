67eecca167a0482c0708860671679ffd41b12e17
mikejianxu <mikejianxu@gmail.com>
2026-02-12 03:22:49 +0800

    tweak header style

--- components/header.tsx
+++ components/header.tsx
@@ -47,8 +47,8 @@ export const Header = () => {
           <Link key={route.href} href={route.href}>
             <Button
               variant={pathname === route.href ? 'default' : 'ghost'}
-              className={cn('h-10 rounded-full min-w-30 justify-start gap-1 pl-1 pr-6 cursor-pointer',
-                pathname === route.href ? 'bg-white': ''
+              className={cn('h-9.5 rounded-full min-w-30 justify-start gap-1 pl-1 pr-6 cursor-pointer',
+                pathname === route.href ? 'bg-white/90': ''
               )}>
               <div className="icon">
                 <route.icon className="h-5! w-5!" />
