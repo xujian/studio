a241c445132e79637c06add218f32cc8bfde442d
mikejianxu <mikejianxu@gmail.com>
2026-03-07 05:10:08 +0800

    remove route log

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,11 @@
 import { type NextRequest } from 'next/server'
 import { updateSession } from '@/lib/supabase/proxy'
-import { logger } from '@/lib/axiom/server'
-import { transformMiddlewareRequest } from '@axiomhq/nextjs'
 
 export async function proxy(request: NextRequest) {
-  // Log incoming request to Axiom
-  logger.info(...transformMiddlewareRequest(request))
-
   if (request.nextUrl.pathname.startsWith('/auth/callback')) {
-    await logger.flush()
     return
   }
-
-  const response = await updateSession(request)
-  await logger.flush()
-  return response
+  return await updateSession(request)
 }
 
 export const config = {
