c3eca4483181f3f5916adc8a64a9f5255afd0723
mikejianxu <mikejianxu@gmail.com>
2026-03-09 03:15:00 +0800

    fix build error

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import type { MDXComponents } from 'mdx/types'
 import { SplitBlock } from './magazine/split-block'
 import { ImageRow } from './magazine/image-row'
 import { InsetImage } from './magazine/inset-image'
 import { PullQuote } from './magazine/pull-quote'
 import { Column } from './magazine/Column'
 
-export const mdxComponents: MDXComponents = {
+export const mdxComponents: Record<string, React.ComponentType<any>> = {
   Column,
   SplitBlock,
   ImageRow,
