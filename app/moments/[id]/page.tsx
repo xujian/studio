bb55507f445aea66dbdc6a69ceccbb6e8221db4e
mikejianxu <mikejianxu@gmail.com>
2026-06-20 02:15:27 +0800

    fxi /moments/[id] width

--- app/moments/[id]/page.tsx
+++ app/moments/[id]/page.tsx
@@ -18,13 +18,17 @@ export default async function MomentPage({
   const { moment, readOnly, author } = await fetchMoment(id)
 
   return (
-    <div className="h-[calc(100vh-6rem)]">
-      <MomentView
-        moment={moment}
-        initialPhotoId={photo || moment.photos[0]?.id}
-        readOnly={readOnly}
-        author={author}
-      />
+    // Break out of the root <main className="container"> width cap so the
+    // moment view can span a wider canvas, then re-center with its own max-width.
+    <div className="relative left-1/2 w-screen -translate-x-1/2">
+      <div className="mx-auto h-[calc(100vh-6rem)] max-w-640 px-4">
+        <MomentView
+          moment={moment}
+          initialPhotoId={photo || moment.photos[0]?.id}
+          readOnly={readOnly}
+          author={author}
+        />
+      </div>
     </div>
   )
 }
